87094a3aa9c6c1d340661f0a9ffac7df92e4c0cb
DavidCortes7700 <davidcortes7700@gmail.com>
2023-05-22 22:39:04 -0500

    first commit

--- src/app/monty-hall/monty-hall.component.ts
+++ src/app/monty-hall/monty-hall.component.ts
@@ -13,6 +13,8 @@ export class MontyHallComponent {
   win: boolean;
   gameOver: boolean;
   selectedDoor: number;
+  gameStats: { attempt: number, change: boolean, win: boolean }[] = [];
+  isStatsModalOpen: boolean = false;
 
   constructor() {
     this.reset();
@@ -54,16 +56,43 @@ export class MontyHallComponent {
       const remainingDoors = [1, 2, 3].filter(
         (door) => door !== this.chosenDoor && door !== this.openDoor
       );
-      this.chosenDoor = remainingDoors.find(
-        (door) => door !== this.selectedDoor
-      );
+      this.chosenDoor = remainingDoors[0];
     }
 
     this.win = this.chosenDoor === this.prizeDoor;
     this.gameOver = true;
+    this.updateStats();
   }
 
   getChangeChoiceText() {
     return this.changeChoice ? 'Sí' : 'No';
   }
+
+  getDoorImage(door: number) {
+    if (this.openDoor === door) {
+      return door === this.prizeDoor ? 'url(../../../../assets/ferrari.png)' : 'url(../../../../assets/cabra.png)';
+    } else {
+      return 'none';
+    }
+  }
+
+  openStatsModal() {
+    this.isStatsModalOpen = true;
+  }
+
+  closeStatsModal() {
+    this.isStatsModalOpen = false;
+  }
+
+  updateStats() {
+    const stat = {
+      attempt: this.gameStats.length + 1,
+      change: this.changeChoice,
+      win: this.win
+    };
+    this.gameStats.push(stat);
+  }
+
+
+
 }
